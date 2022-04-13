0c92d76ed4a08111690f08f22713706da4f78254
ZuhChar <zcrimmel@uwyo.edu>
2022-04-12 20:24:53 -0600

    L

---
+++ wytalkC.c
@@ -0,0 +1,15 @@
+// 
+//  wytalkC.c
+//  Author: Zachary Crimmel
+//  Date: Apr 12, 2022
+//
+//  COSC 3750, Homework 7
+//
+//  This is the client side talk program that will connect to wytalkD host on port 51100.
+//  Responsible for reading from the user and sending a line of user input to wytalkD
+//  Collaborated with Ian Moon on this Homework
+//
+
+int main(){
+
+}
---
+++ wytalkD.c
@@ -0,0 +1,14 @@
+// 
+// wytalkD.c
+// Author: Zachary Crimmel
+// Date: Apr 12, 2022
+//
+// COSC 3750, Homework 7
+//
+// This is the server side of the program responsible for opening the port and listening to the client.
+// Collaborated with Ian Moon on this Homework
+//
+
+int main(){
+
+}
