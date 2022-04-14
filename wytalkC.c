4ae417fe038a42b2525441a977a0625c84b2a142
ZuhChar <zcrimmel@uwyo.edu>
2022-04-14 13:52:45 -0600

    l

--- wytalkC.c
+++ wytalkC.c
@@ -15,23 +15,51 @@
 
 int main()
 {
-	char buffer;
-	int socket = 51100, valread;
+	char buffer[128];
+	int socket = 51100;
 	int length = 128;
 	int flags = 0;
-
+	char ch;
+	int index;
 	int conft = 0;
+	char *rtn;
+	char *frtn;
 
-	conft = request_connection("fish11", 51100);
+	conft = request_connection(gethostname(), 51100);
 	printf("connection made \n");
 	while (1)
 	{
-		scanf("%s", buffer);
-		printf("Your message:%s", buffer);
-		send(socket, &buffer, length, flags);
-		if (sizeof(buffer) == 0)
+		frtn = fgets(buffer, length, stdin);
+		if(frtn < 0){
 			break;
+		}
+		write(socket, buffer, length);
+		while (1)
+		{
+			rtn = read(socket, &ch, 1);
+			if (rtn < 0)
+			{
+				close(51100);
+				return 0;
+			}
+			buffer[index] = ch;
+			if (ch == "\n")
+			{
+				break;
+			}
+			if (index == size - 2)
+			{
+				break;
+			}
+			// scanf("%s", buffer);
+			// printf("Your message:%s", buffer);
+			// send(socket, &buffer, length, flags);
+			// if (sizeof(buffer) == 0)
+			// 	break;
+		}
+		printf("%s", buffer);
 	}
 	close(51100);
 	printf("port closed\n");
+	return 0;
 }
