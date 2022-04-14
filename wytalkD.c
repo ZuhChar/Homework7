3266a22c37a7caba4dc4dab674b6ead68900247d
ZuhChar <zcrimmel@uwyo.edu>
2022-04-13 18:44:50 -0600

    l

--- wytalkD.c
+++ wytalkD.c
@@ -8,6 +8,7 @@
 // This is the server side of the program responsible for opening the port and listening to the client.
 // Collaborated with Ian Moon on this Homework
 //
+
 #include "socketfun.h"
 
 int main()
@@ -17,7 +18,7 @@ int main()
     int size = 128;
     ssize_t message = 0;
     int sfd = 0;
-    char host = "fish";
+    char host = 51100;
     int confd = 0;
 
     sfd = serve_socket(host, 51100);
